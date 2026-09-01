a9759e456ec3e55dc1fd73b71d0d1bc5c1f9370c
ahmadrka <arlpenik02@gmail.com>
2026-09-01 16:42:29 +0700

    fix: module can't read dotenv configuration.

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config';
 import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '../generated/prisma/client';
 import { PrismaLibSql } from '@prisma/adapter-libsql';
